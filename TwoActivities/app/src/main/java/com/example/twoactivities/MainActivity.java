6664b318a6cadadfbb4a335b33c44595d1a4b09a
Aditya8014 <adityapk60@gmail.com>
2021-03-17 16:51:38 +0545

    add a constant for the LOG_TAG variable

--- TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG=MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
