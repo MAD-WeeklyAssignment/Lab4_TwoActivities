1d1c79fbb47fbf4a45b1463dbdf0acead0c606e7
Aditya8014 <adityapk60@gmail.com>
2021-03-18 13:03:06 +0545

    create sayHello method to implement Intent

--- TwoActivities_HomeWork/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ TwoActivities_HomeWork/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -1,62 +1,31 @@
 package com.example.twoactivities;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG=MainActivity.class.getSimpleName();
-    public static final String EXTRA_MESSAGE ="com.example.android.twoactivities.extra.MESSAGE";
-    private EditText mMessageEditText;
-    public static final int TEXT_REQUEST = 1;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
+    public static final String COUNT_EXTRA = "com.example.twoactivities.extra.COUNT";
+    private TextView mCount;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.d(LOG_TAG,"-------------");
-        Log.d(LOG_TAG,"onCreate");
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
+        mCount = findViewById(R.id.count);
+        count = Integer.parseInt(mCount.getText().toString());
     }
 
-    @Override
-    public void onStart(){
-        super.onStart();
-        Log.d(LOG_TAG, "onStart");
-    }
-
-
-
-    public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
+    public void sayHello(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivityForResult(intent, TEXT_REQUEST);
+        intent.putExtra(COUNT_EXTRA, count);
+        startActivity(intent);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
-            }
-        }
-    }
+   
 }
