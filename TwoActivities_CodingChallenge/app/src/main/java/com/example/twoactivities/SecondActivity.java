108bc63b57c3a473139b6db0b5569919a1137065
Aditya8014 <adityapk60@gmail.com>
2021-03-18 10:35:36 +0545

    implement method to display clicked text

--- TwoActivities_CodingChallenge/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ TwoActivities_CodingChallenge/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -1,33 +1,30 @@
 package com.example.twoactivities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class SecondActivity extends AppCompatActivity {
-    public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
-    private EditText mReply;
+
+    private TextView mTextPassage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        mReply = findViewById(R.id.editText_second);
-        Intent intent =getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
-    }
+        mTextPassage = findViewById(R.id.text_passage);
+        Intent intent = getIntent();
+        int textPassageNumber = intent.getIntExtra(MainActivity.TEXT_PASSAGE_NO, 1);
 
-    public void returnReply(View view) {
-        String reply = mReply.getText().toString();
-        Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_REPLY, reply);
-        setResult(RESULT_OK, replyIntent);
-        finish();
+        if (textPassageNumber == 1) {
+            mTextPassage.setText(R.string.text_passage_one);
+        } else if (textPassageNumber == 2) {
+            mTextPassage.setText(R.string.text_passage_two);
+        } else if (textPassageNumber == 3) {
+            mTextPassage.setText(R.string.text_passage_three);
+        }
+
     }
 }
